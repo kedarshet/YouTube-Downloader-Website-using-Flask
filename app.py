d73162dee34c42197e85dc428366dc0b5f6a9860
Kedar U Shet <62643927+kedarshet@users.noreply.github.com>
2021-12-19 12:12:13 +0530

    Fixed Bugs.

--- app.py
+++ app.py
@@ -9,40 +9,48 @@ app=Flask(__name__)
 app.config['SECRET_KEY'] = 'kedar'
 @app.route('/')
 def home():
-    html = urllib.request.urlopen("https://www.youtube.com/feed/explore")
-    videos = re.findall(r"watch\?v=(\S{11})",html.read().decode())
-    for i in range(9):
-        videos[i] = "https://www.youtube.com/watch?v="+str(videos[i])
-    trend_list = videos[:9]
-    for i in range(len(trend_list)):
-        session[str(i)] = trend_list[i]
-        trend_list[i]=YouTube(session[str(i)])
-    return render_template('index.html',list=trend_list,)
+    try:    
+        html = urllib.request.urlopen("https://www.youtube.com/feed/explore")
+        videos = re.findall(r"watch\?v=(\S{11})",html.read().decode())
+        for i in range(9):
+            videos[i] = "https://www.youtube.com/watch?v="+str(videos[i])
+        trend_list = videos[:9]
+        for i in range(len(trend_list)):
+            session[str(i)] = trend_list[i]
+            trend_list[i]=YouTube(session[str(i)])
+        return render_template('index.html',list=trend_list)
+    except:
+        return redirect(url_for('home'))
 
 @app.route('/static/',methods=['GET', 'POST'])
 def downloads():
     if request.method == 'POST':
-        if request.form.get("url"):
-            url=request.form.get("url")
-            session['link'] = url
-            url = YouTube(session['link'])  
-        else:
-            ind = request.form.get("url1")
-            session['link']=session[str(ind)]
-            url = YouTube(session['link'])  
-        return render_template('download.html',  url=url)
-        
+        try:
+            if request.form.get("url"):
+                url=request.form.get("url")
+                session['link'] = url
+                url = YouTube(session['link'])  
+            else:
+                ind = request.form.get("url1")
+                session['link']=session[str(ind)]
+                url = YouTube(session['link'])             
+            return render_template('download.html',  url=url)
+        except:
+            return redirect(url_for('home'))
 #filetitle = url.title, filepath = filepath,
 @app.route('/download/',methods=['GET', 'POST'])
 def download_video():
-    if request.method== 'POST':
-        url = YouTube(session['link'])
-        itag = request.form.get('itag')
-        filepath = request.form.get('filepath')
-        video = url.streams.get_by_itag(itag)
-        filepath = video.download()
-        return send_file(filepath, as_attachment=True)
-    return render_template('index.html',value='home')
+    try:
+        if request.method== 'POST':
+            url = YouTube(session['link'])
+            itag = request.form.get('itag')
+            filepath = request.form.get('filepath')
+            video = url.streams.get_by_itag(itag)
+            filepath = video.download()
+            return send_file(filepath, as_attachment=True)
+        return render_template('index.html',value='home')
+    except:
+        return redirect(url_for('home'))
             
 if __name__ == '__main__':
     app.run(debug=False,host='0.0.0.0')
